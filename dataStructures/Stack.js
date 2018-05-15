570f40fe6cfb2bdb2ba5d5b25023eb632f6ce061
Steven Isbell <steven.isbell18@gmail.com>
2018-05-15 09:51:01 -0500

    added stack

---
+++ dataStructures/Stack.js
@@ -0,0 +1,29 @@
+// A stack can be more or less implemented using a standard JS Array.
+// This is an implementation that brings standard array functionality
+// into a single structure.
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(val) {
+    this.items.push(val);
+  }
+  pop() {
+    if (this.items.length == 0) return 'Underflow';
+    return this.items.pop();
+  }
+  currentItem() {
+    return this.items[this.items.length - 1];
+  }
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  printStack() {
+    let str = '';
+    for (var i = 0; i < this.items.length; i++) {
+      str += `${this.items[i]} `;
+    }
+    return str;
+  }
+}
