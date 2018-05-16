638a8c5a5651275c3657b7c0cc7ef01adc149fa5
Steven Isbell <steven.isbell18@gmail.com>
2018-05-15 22:04:22 -0500

    added hashmap

---
+++ dataStructures/HashTable.js
@@ -0,0 +1,69 @@
+// A Hash Table (Hash Map) is a data structure used to implement an associative array,
+// a structure that can map keys to values.
+// A Hash Table uses a hash function to compute an index into an array of
+// buckets or slots, from which the desired value can be found.
+
+const hash = (string, max) => {
+  let hash = 0;
+  for (let i = 0; i < string.length; i++) {
+    hash += string.charCodeAt(i);
+  }
+  return hash % max;
+};
+
+class HashTable {
+  constructor(limit = 10) {
+    this.storage = [];
+    this.storageLimit = limit;
+  }
+
+  print() {
+    console.log(this.storage);
+  }
+
+  add(key, value) {
+    const index = hash(key, storageLimit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
+  }
+
+  remove(key) {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
+      delete this.storage[index];
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          delete this.storage[index][i];
+        }
+      }
+    }
+  }
+
+  lookup(key) {
+    const index = hash(key, this.storageLimit);
+    if (this.storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          return this.storage[index][i][1];
+        }
+      }
+    }
+  }
+}
+
+const myHashTable = new HashTable();
