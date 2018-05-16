c996cf64c12eb46a93ece99dd70e3ff0e672b9f5
Steven Isbell <steven.isbell18@gmail.com>
2018-05-16 09:30:47 -0500

    comments

--- sortingAlgos/insertionSort.js
+++ sortingAlgos/insertionSort.js
@@ -1,3 +1,9 @@
+// Insertion sort iterates, consuming one input element each repetition,
+// and growing a sorted output list.
+// Each iteration removes one element from the input data,
+// finds the location it belongs within the sorted list,
+// and inserts it there. It repeats until no input elements remain.
+
 function insertionSort(arr) {
   let i;
   let j;
